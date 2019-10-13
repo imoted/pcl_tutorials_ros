1b6f2e79a09d2c940b16126be402fa6382395f79
ShotaAk <macakasit@gmail.com>
2019-10-14 00:14:12 +0900

    Update test node

--- test/test_filtering_node.py
+++ test/test_filtering_node.py
@@ -9,10 +9,10 @@ import time
 class FilteringNodeTest(unittest.TestCase):
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
-        self.assertIn('pcl_tutorials_ros', nodes, 'node does not exist')
+        self.assertIn('/example_filtering_', nodes, 'node does not exist')
 
 if __name__ == '__main__':
     # Wait for node startup
     time.sleep(3)
-    rospy.init_node('example_filtering_')
+    rospy.init_node('test_filtering_node')
     rostest.rosrun('pcl_tutorials_ros', 'test_filtering_node', FilteringNodeTest)
