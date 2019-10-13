d266530a3b60eadfbe8e91bc8c55216285b82d88
ShotaAk <macakasit@gmail.com>
2019-10-13 23:24:13 +0900

    Add rostest file

---
+++ test/test_filtering_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#encoding: utf8
+
+import rospy, unittest, rostest
+import rosnode
+import time
+
+
+class FilteringNodeTest(unittest.TestCase):
+    def test_node_exist(self):
+        nodes = rosnode.get_node_names()
+        self.assertIn('pcl_tutorials_ros', nodes, 'node does not exist')
+
+if __name__ == '__main__':
+    # Wait for node startup
+    time.sleep(3)
+    rospy.init_node('example_filtering')
+    rostest.rosrun('pcl_tutorials_ros', 'test_filtering_node', FilteringNodeTest)
